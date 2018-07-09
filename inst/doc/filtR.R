fdb9da64558958b25d4842f08bd28c34af957f98
Ben Joris <bjoris@uwo.ca>
2018-07-09 12:53:46 -0400

    Vignette hyperlinking fix

--- inst/doc/filtR.R
+++ inst/doc/filtR.R
@@ -15,3 +15,9 @@ filtered <- filtR('SRP078943_taxonomy_abundances_SSU_v4.1.tsv', rho_CO = 0.6)
 ## ------------------------------------------------------------------------
 filtered <- filtR('SRP078943_taxonomy_abundances_SSU_v4.1.tsv', clr_CO = 4)
 
+## ---- error = TRUE, results = FALSE--------------------------------------
+filtered <- filtR('SRP078943_taxonomy_abundances_SSU_v4.1.tsv', rho_CO = 0.99)
+
+## ---- error = TRUE, results = FALSE--------------------------------------
+filtered <- filtR('SRP078943_taxonomy_abundances_SSU_v4.1.tsv', clr_CO = 50)
+
